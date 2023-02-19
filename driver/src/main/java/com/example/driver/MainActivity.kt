636de57e5fd85e52ecaf62cd5add1c8cead5fdb7
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-02-19 17:21:51 +0530

    Android compose

--- driver/src/main/java/com/example/driver/MainActivity.kt
+++ driver/src/main/java/com/example/driver/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.driver
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,26 +22,47 @@ class MainActivity : ComponentActivity() {
         setContent {
             First_Mobile_AppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                TwoGreetings()
             }
         }
     }
 }
+@Composable
+fun TwoGreetings(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column() {
+        Greeting("Pathum")
+        Greeting("Sanjana")
+        Button(onClick = {
+            Log.d("TwoGreetings","You clicked")
+        }) {
+            Text(text = "Click on this")
+        }    
+        }
+    }
+}
+
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name Welcome!")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreviewLight() {
     First_Mobile_AppTheme {
-        Greeting("Android")
+        TwoGreetings()
     }
 }
+
+/*@Preview(showBackground = true)
+@Composable
+fun DefaultPreviewDark() {
+    First_Mobile_AppTheme(darkTheme = true) {
+        Greeting("Pathum Sanjana")
+    }
+}*/
